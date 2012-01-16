aebbb9206d8973db9f7fbb46b61a62b88ec314f7
Puneet Singh <puneet.mir@gmail.com>
2012-01-16 10:49:21 +0530

    About Triangle Project Done

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  result = if(a==b && b==c)
+		:equilateral
+	   elsif((a==b && a!=c)||(a==c&&a!=b)||(b==c&&b!=a))
+		:isosceles
+	   else
+		:scalene
+	   end	
+	   
 end
 
 # Error class used in part 2.  No need to change this code.
